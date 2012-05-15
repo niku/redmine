d4f3f77a1b136dfdc110fd07cf2ee1dfee4c44b2
niku <niku@niku.name>
2012-05-15 15:15:42 +0900

    add fcgi

--- fluxflex_deploy.sh
+++ fluxflex_deploy.sh
@@ -4,6 +4,7 @@ export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.
 export RAILS_ENV=production
 
 # FIXME error occured unless install openid
-bundle install --without development test ldap rmagick postgresql sqlite # openid
+bundle install --path vendor/bundle \
+--without development test ldap rmagick postgresql sqlite # openid
 
 bundle exec rake db:migrate
--- fluxflex_setup.sh
+++ fluxflex_setup.sh
@@ -3,6 +3,8 @@ export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 export RAILS_ENV=production
 
+gem install fcgi --no-ri --no-rdoc
 gem install bundler --no-ri --no-rdoc
 # FIXME error occured unless install openid
-bundle install --without development test ldap rmagick postgresql sqlite # openid
+bundle install --path vendor/bundle \
+--without development test ldap rmagick postgresql sqlite # openid
